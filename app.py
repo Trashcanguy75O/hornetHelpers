9485e0fc0eedeb242f4f97d688265ec0e40d466b
Trashcanguy75O <elliottea15@gmail.com>
2026-04-22 17:19:58 -0700

    Initial commit for the account editor. It has the UI and database kinda integrated into it but is a currently rudimentary implementation to get things rolling. Need to discuss database more to see what needs to be done (currently just uses a test user account) and how it's all supposed to work. It also has a new base for applications called app_base.html which serves to be the index.html basically but for post-login UI vs. during-login UI. (contrast to the current base.html)

--- app.py
+++ app.py
@@ -1,26 +1,65 @@
-from flask import Flask, render_template
-#from acc_recovery import hello_world
+from flask import Flask, render_template, request
+from DBMethods import UserRepository
 
 app = Flask(__name__)
+
+repo = UserRepository("hornet_helpers.db")
+repo.initialize()
+
+# Temporary scaffold for testing until login/session is connected
+if repo.find_user("testuser") is None:
+    repo.add_user("testuser", "password123", "Test User", "testuser@csus.edu")
+
+def get_current_username():
+    return "testuser" # This should also be replaced
 
 @app.route("/homepage")
 def home():
     return render_template("homepage.html")
+
+
 @app.route("/acc_login")
 def login():
     return render_template("acc_login.html")
+
 
 @app.route("/new_account")
 def new_account():
     return render_template("new_account.html")
 
+
 @app.route("/forgot_username")
 def username():
     return render_template("forgot_username.html")
 
+
 @app.route("/forgot_password")
 def password():
     return render_template("forgot_password.html")
+
+
+@app.route("/account")
+def account():
+    username = get_current_username()
+    user = repo.find_user(username)
+    return render_template("account.html", user=user)
+
+@app.route("/account/update", methods=["POST"])
+def update_account():
+    username = get_current_username()
+    full_name = request.form["full_name"]
+    email = request.form["email"]
+
+    success, message = repo.update_user(username, full_name, email)
+    user = repo.find_user(username)
+
+    return render_template(
+        "account.html",
+        user=user,
+        message=message,
+        success=success
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
